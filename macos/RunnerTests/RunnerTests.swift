7a66c18175e388a9d6ececdc1ba92739e1584485
shenlan <manbuzhe2009@qq.com>
2025-07-08 06:06:27 +0800

    Add unit tests and CI

--- macos/RunnerTests/RunnerTests.swift
+++ macos/RunnerTests/RunnerTests.swift
@@ -1,12 +1,21 @@
 import Cocoa
 import FlutterMacOS
+@testable import Runner
 import XCTest
 
 class RunnerTests: XCTestCase {
 
-  func testExample() {
-    // If you add code to the Runner application, consider adding tests here.
-    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  func testRunShellScriptSuccess() {
+    let delegate = AppDelegate()
+    let expectation = XCTestExpectation(description: "shell")
+    delegate.runShellScript(command: "echo hello", returnsBool: false) { result in
+      if let str = result as? String {
+        XCTAssertEqual(str, "success")
+      } else {
+        XCTFail("Unexpected result")
+      }
+      expectation.fulfill()
+    }
+    wait(for: [expectation], timeout: 5.0)
   }
-
 }
